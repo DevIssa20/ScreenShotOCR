2f21561a69121a3e9be7fa6646c0132549420825
DevIssa20 <issa2002farhat@outlook.com>
2023-12-08 13:58:17 +0200

    Added automatic summarization functionality

--- ScreenShotOCRSummary.py
+++ ScreenShotOCRSummary.py
@@ -1,14 +1,28 @@
 from PIL import Image, ImageGrab
 import cv2
+import os
 import pytesseract
 import keyboard
 import time
 import pyperclip
 import numpy as np
 from collections import Counter
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
 
 tesseract_path = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
 pytesseract.pytesseract.tesseract_cmd = tesseract_path
+
+import os
+
+# Load environment variables from file
+with open('.env') as f:
+    for line in f:
+        key, value = line.strip().split('=')
+        os.environ[key] = value
+
+# Use environment variables in your code
+print(os.environ["OPENAI_API_KEY"])
 
 def on_key_event(e): #Waits for shift + ctrl combination to start OCR process
     if e.event_type == keyboard.KEY_DOWN:
@@ -19,9 +33,16 @@ def on_key_event(e): #Waits for shift + ctrl combination to start OCR process
                 img = processImage(screenshot,bgColor)
                 extractText(img)
                 print("Screenshot captured from clipboard and processed!")
-                time.sleep(1)
+                time.sleep(0.5)
+                summarizeText(pyperclip.paste()) #Comment out to disable summarization
+                print("Screenshot text summarized!") #Comment out to disable summarization
             else:
-                print("No image found in clipboard.")
+                text = pyperclip.paste()
+                if(text == None):
+                    print("Neither an image nor text was found in the clipboard")
+                    return
+                summarizeText(text) #Comment out to disable summarization
+                print("Text copied from clipboard and summarized!") #Comment out to disable summarization
                 
 def detect_background_color(image): #Detects background color of the image and returns that information to be used in preprocessing
     img = np.array(image)
@@ -53,8 +74,17 @@ def extractText(img): #Extracts text from image, automatically saves it into the
     pyperclip.copy(ocr_result)
     return ocr_result
 
+def summarizeText(text): #Summarizes OCR text by sending sending the information to GPT-3.5 and asking for a summary
+    llm = OpenAI(model_name="text-davinci-003", openai_api_key = os.environ['OPENAI_API_KEY'])
+    template = """
+    You will be provided with some text captured from a screenshot using OCR. I want you to summarize this text. The text may include some grammatical mistakes.
+    The text you have to summarize is: {text}
+    """
+    prompt = PromptTemplate(input_variables=["text"], template=template)
+    pyperclip.copy(llm(prompt.format(text=text)))
+
 def main():
-    print("Press Shift + Ctrl to load image from clipboard.\n"+
+    print("Press Shift + Ctrl to load image from clipboard. Press Shift + Ctrl + alt to perform summarization on image or text in clipboard\n"+
           "You can keep the program running in the background, it'll terminate when you press esc")
     keyboard.hook(on_key_event)
     try:
